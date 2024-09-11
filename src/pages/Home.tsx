8a2c6f389fff7d9ec56db104ca0c153f0f622e7c
Marcele Cardozo <marcelecardozo@gmail.com>
2024-09-11 19:49:50 -0300

    feat: add react hook form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,53 @@
-import { useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const registerSchema = z.object({
+  name: z.string().min(4),
+  lastName: z.string(),
+});
+
+type RegisterShcema = z.infer<typeof registerSchema>;
 
 function Home() {
-  const [message, setMessage] = useState<string>("Carregando...");
+  const { register, handleSubmit } = useForm<RegisterShcema>({
+    resolver: zodResolver(registerSchema),
+  });
 
-  useEffect(() => {
-    setMessage("Bem-vindo ao meu projeto bonito!");
-  }, []);
+  function handleSubmitForm(data: RegisterShcema) {
+    console.log(data);
+  }
 
   return (
-    <div>
-      <p>Meu Projeto Bonito</p>
-      <p>{message}</p>
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
+      <h1 className="text-4xl font-bold mb-4">Cadastro</h1>
+      <form
+        onSubmit={handleSubmit(handleSubmitForm)}
+        className="space-y-4 w-full max-w-md"
+      >
+        <TextField
+          id="name"
+          label="Nome"
+          variant="standard"
+          className="w-full bg-white rounded-md"
+          {...register("name")}
+        />
+        <TextField
+          id="lastName"
+          label="Sobrenome"
+          variant="standard"
+          className="w-full bg-white rounded-md"
+          {...register("lastName")}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          className="w-full bg-white rounded-md"
+        >
+          Enviar
+        </Button>
+      </form>
     </div>
   );
 }
